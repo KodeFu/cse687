2631a4f98260fea14a4dd9b542ed65386b7f53eb
Mudit Vats <muditv@gmail.com>
2020-04-27 20:11:33 -0700

    Change API definition.

--- TestFuncsA/TestFuncsA/TestFuncsA.h
+++ TestFuncsA/TestFuncsA/TestFuncsA.h
@@ -1,28 +1,28 @@
 #pragma once
 
 #ifdef TESTFUNCSA_EXPORTS
-#define MATHLIBRARY_API __declspec(dllexport)
+#define TESTFUNCSA_API __declspec(dllexport)
 #else
-#define MATHLIBRARY_API __declspec(dllimport)
+#define TESTFUNCSA_API __declspec(dllimport)
 #endif
 
 // Test1
-extern "C" MATHLIBRARY_API bool Test1();
+extern "C" TESTFUNCSA_API bool Test1();
 
 // Test2
-extern "C" MATHLIBRARY_API bool Test2();
+extern "C" TESTFUNCSA_API bool Test2();
 
 // Test3
-extern "C" MATHLIBRARY_API bool Test3();
+extern "C" TESTFUNCSA_API bool Test3();
 
 // Test4
-extern "C" MATHLIBRARY_API bool Test4();
+extern "C" TESTFUNCSA_API bool Test4();
 
 // Test5
-extern "C" MATHLIBRARY_API bool Test5();
+extern "C" TESTFUNCSA_API bool Test5();
 
 // Test6
-extern "C" MATHLIBRARY_API bool Test6();
+extern "C" TESTFUNCSA_API bool Test6();
 
 // Test7
-extern "C" MATHLIBRARY_API bool Test7();
+extern "C" TESTFUNCSA_API bool Test7();
