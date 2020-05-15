f6e74903fd768f3e9806554fa7faf5a85b0af31c
dlh4596 <dlhowick@syr.edu>
2020-05-14 17:16:53 -0700

    Hey, this is stuff I'm working.  Still in progress

--- TestHarness/TestHarness/TestHarness.cpp
+++ TestHarness/TestHarness/TestHarness.cpp
@@ -16,7 +16,9 @@
 
 #include <iostream>
 #include <string>
-#include <windows.h>
+#include <Windows.h>
+#include <thread>
+#include <mutex>
 #include "TestExecutive.h"
 #include "TestHarness.h"
 #include "TestRequestQueue.h"
@@ -24,12 +26,69 @@
 #include "ChildTest.h"
 #include "ThreadMessageQueue.h"
 
+using namespace std;
+
+std::mutex displayMutex;
+// prototype for the test functions
+typedef bool(__stdcall* testFunc)();
 
 TestHarness::TestHarness()
 {
 	mLogMsgQueue = new ThreadMessageQueue<Message>;
 	mLogger = new Logger(mLogMsgQueue);
 }
+
+class Thread_Test_obj
+{
+public:
+	void operator() (std::string dllName, std::string testName)
+	{
+		std::lock_guard<std::mutex> l(displayMutex);
+		{
+			cout << "using thread " << std::this_thread::get_id() << " using callable with dll: " << dllName << " and test: " << testName << endl;
+			test(dllName, testName);
+		}
+	}
+
+	bool test(std::string testDllName, std::string testDllFuncName)
+	{
+		bool result;
+		std::wstring wTestDllName(testDllName.begin(), testDllName.end());
+		LPCWSTR lpcwstrTestDllName = wTestDllName.c_str();
+		LPCSTR lpcstrTestDllFuncName;
+
+		// get a handle to the test dll
+		// displayMutex.lock();
+		HINSTANCE hTestDll = LoadLibrary(lpcwstrTestDllName);
+		if (hTestDll != NULL)
+		{
+			// get a pointer to the function
+			// testDllFuncName = tq.dequeueTest();
+			lpcstrTestDllFuncName = testDllFuncName.c_str();
+			testFunc pTestFunc = (testFunc)GetProcAddress(hTestDll, lpcstrTestDllFuncName);
+			if (NULL != pTestFunc)
+			{
+				// run the function
+				result = pTestFunc();
+				std::cout << "TestHarness: " << testDllName << " Test Function: " << testDllFuncName
+					<< " returned " << (result ? "True" : "False") << " in the Child Test Class." << std::endl;
+			}
+			else
+			{
+				std::cout << "TestHarness: " << testDllName << " Couldn't find test function." << std::endl;
+			}
+
+			FreeLibrary(hTestDll);
+			return result;
+		}
+		else
+		{
+			std::cout << "TestHarness: Could Not find DLL: " << testDllName << std::endl;
+		}
+		// displayMutex.unlock();
+	}
+};
+
 
 int main()
 {
@@ -56,6 +115,7 @@ int main()
 	tq.Enqueue(msg);
 
 	// While (Hardcoded to be true for now)
+	/*
 	while (true)
 	{
 		// Grab the first message off the queue, this is a blocking call
@@ -94,5 +154,35 @@ int main()
 			std::cout << "TestHarness: Could Not find DLL: " << testDllName << std::endl;
 		}
 	}
+	*/
+	// Grab the first message off the queue, this is a blocking call
+	Message msgD = tq.Dequeue();
+
+	// Declare string variables for holding DLL and Function Name
+	std::string testDllName = msgD.filePath;
+	std::string testDllFuncName = msgD.functionName;
+	thread t1(Thread_Test_obj(), testDllName, testDllFuncName);
+
+	msgD = tq.Dequeue();
+	testDllName = msgD.filePath;
+	testDllFuncName = msgD.functionName;
+	thread t2(Thread_Test_obj(), testDllName, testDllFuncName);
+
+	msgD = tq.Dequeue();
+	testDllName = msgD.filePath;
+	testDllFuncName = msgD.functionName;
+	thread t3(Thread_Test_obj(), testDllName, testDllFuncName);
+
+	// wait for em
+	// t1.join();
+	// t2.join();
+	// t3.join();
+
+	t1.detach();
+	t2.detach();
+	t3.detach();
+	cout << "<return" << endl;
+	cin.get();
+
 }
 
