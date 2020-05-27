de32371dca0169c7ea82ff00022ad91e770462c1
Mudit Vats <muditv@gmail.com>
2020-05-27 12:12:48 -0700

    Cleaned up unused files. Implemented simple thread pool.

--- TestFuncsA/TestFuncsA/TestFuncsA.cpp
+++ TestFuncsA/TestFuncsA/TestFuncsA.cpp
@@ -5,21 +5,21 @@
 // Test1: just returns true
 bool Test1()
 {
-	std::cout << "TestFuncsA.dll: Hello from Test1 function." << std::endl;
+	std::cout << "(" << GetCurrentThreadId() << ") " << "TestFuncsA.dll: Hello from Test1 function." << std::endl;
 	return true;
 }
 
 // Test2: just returns false
 bool Test2()
 {
-	std::cout << "TestFuncsA.dll: Hello from Test2 function." << std::endl;
+	std::cout << "(" << GetCurrentThreadId() << ") " << "TestFuncsA.dll: Hello from Test2 function." << std::endl;
 	return false;
 }
 
 // Test3: throws and exception
 bool Test3()
 {
-	std::cout << "TestFuncsA.dll: Hello from Test3 function." << std::endl;
+	std::cout << "(" << GetCurrentThreadId() << ") " << "TestFuncsA.dll: Hello from Test3 function." << std::endl;
 
 	throw 0xdead;
 
@@ -29,7 +29,7 @@ bool Test3()
 // Test4: takes some time; 5 seconds
 bool Test4()
 {
-	std::cout << "TestFuncsA.dll: Hello from Test4 function." << std::endl;
+	std::cout << "(" << GetCurrentThreadId() << ") " << "TestFuncsA.dll: Hello from Test4 function." << std::endl;
 
 	Sleep(1000 * 5);
 
@@ -40,7 +40,7 @@ bool Test4()
 // Test5: takes some time; 15 seconds
 bool Test5()
 {
-	std::cout << "TestFuncsA.dll: Hello from Test5 function." << std::endl;
+	std::cout << "(" << GetCurrentThreadId() << ") " << "TestFuncsA.dll: Hello from Test5 function." << std::endl;
 
 	Sleep(1000 * 15);
 
@@ -50,7 +50,7 @@ bool Test5()
 // Test6: takes some time; 10 seconds; throws exception
 bool Test6()
 {
-	std::cout << "TestFuncsA.dll: Hello from Test6 function." << std::endl;
+	std::cout << "(" << GetCurrentThreadId() << ") " << "TestFuncsA.dll: Hello from Test6 function." << std::endl;
 
 	Sleep(1000 * 10);
 
@@ -63,7 +63,7 @@ bool Test6()
 // Test4: takes some time; 20 seconds; throws exception
 bool Test7()
 {
-	std::cout << "TestFuncsA.dll: Hello from Test7 function." << std::endl;
+	std::cout << "(" << GetCurrentThreadId() << ") " << "TestFuncsA.dll: Hello from Test7 function." << std::endl;
 
 	Sleep(1000 * 20);
 
--- TestHarness/TestHarness/TestHarness.cpp
+++ TestHarness/TestHarness/TestHarness.cpp
@@ -17,193 +17,100 @@
 #include "ProcessMessageQueue.h"
 #include <iostream>
 #include <string>
-#include <Windows.h>
 #include <thread>
-#include <mutex>
-#include "TestExecutive.h"
 #include "TestHarness.h"
-#include "TestRequestQueue.h"
-#include "LogOutputQueue.h"
-#include "ChildTest.h"
 #include "ThreadMessageQueue.h"
-
 
 using namespace std;
 
-std::mutex displayMutex;
+#define THREAD_POOL_MAX 5 // number of threads in the thread pool
+
+// childTester function
+void childTester(ProcessMessageQueue& testQueue);
+
 // prototype for the test functions
 typedef bool(__stdcall* testFunc)();
-
-TestHarness::TestHarness()
-{
-	mLogMsgQueue = new ThreadMessageQueue<Message>;
-	mLogger = new Logger(mLogMsgQueue);
-}
-
-class Thread_Test_obj
-{
-public:
-	void operator() (std::string dllName, std::string testName)
-	{
-		std::lock_guard<std::mutex> l(displayMutex);
-		{
-			cout << "using thread " << std::this_thread::get_id() << " using callable with dll: " << dllName << " and test: " << testName << endl;
-			test(dllName, testName);
-		}
-	}
-
-	bool test(std::string testDllName, std::string testDllFuncName)
-	{
-		bool result;
-		std::wstring wTestDllName(testDllName.begin(), testDllName.end());
-		LPCWSTR lpcwstrTestDllName = wTestDllName.c_str();
-		LPCSTR lpcstrTestDllFuncName;
-
-		// get a handle to the test dll
-		// displayMutex.lock();
-		HINSTANCE hTestDll = LoadLibrary(lpcwstrTestDllName);
-		if (hTestDll != NULL)
-		{
-			// get a pointer to the function
-			// testDllFuncName = tq.dequeueTest();
-			lpcstrTestDllFuncName = testDllFuncName.c_str();
-			testFunc pTestFunc = (testFunc)GetProcAddress(hTestDll, lpcstrTestDllFuncName);
-			if (NULL != pTestFunc)
-			{
-				// run the function
-				result = pTestFunc();
-				std::cout << "TestHarness: " << testDllName << " Test Function: " << testDllFuncName
-					<< " returned " << (result ? "True" : "False") << " in the Child Test Class." << std::endl;
-			}
-			else
-			{
-				std::cout << "TestHarness: " << testDllName << " Couldn't find test function." << std::endl;
-			}
-
-			FreeLibrary(hTestDll);
-			return result;
-		}
-		else
-		{
-			std::cout << "TestHarness: Could Not find DLL: " << testDllName << std::endl;
-		}
-		// displayMutex.unlock();
-	}
-};
 
 
 int main()
 {
-	// Test ProcessMessageQueue
-	// Test ProcessMessageQueue
-	// Test ProcessMessageQueue
+	// wait for TestExec - do this in another thread?
 	cout << "Waiting for TestExec() to connect.... " << endl;
-	ProcessMessageQueue queue;
-	queue.ServerListen("127.0.0.1", 5005);
+	ProcessMessageQueue testQueue;
+	testQueue.ServerListen("127.0.0.1", 5005);
 
-	while (true)
-	{
-		if (!queue.isEmpty())
-		{
-			Message procMsg = queue.Dequeue();
-			cout << procMsg.filePath << endl;
-			cout << procMsg.functionName << endl;
-		}
-	}
-	// Test ProcessMessageQueue
-	// Test ProcessMessageQueue
-	// Test ProcessMessageQueue
-
-	// TestRequestQueue tq;
-	ThreadMessageQueue<Message> tq;
-	ThreadMessageQueue<Message>* logQueue = new ThreadMessageQueue<Message>;
-
-	Message msg;
-	
 	// Since this class isn't object oriented at the moment, just make a local logger
-	Logger* log = new Logger(logQueue);
+	//ProcessMessageQueue logQueue;
+	//Logger* log = new Logger(logQueue);
 
-	msg.filePath = "TestFuncsA.dll";
-	msg.functionName = "Test1";
-
-	tq.Enqueue(msg);
-
-	msg.filePath = "TestFuncsA.dll";
-	msg.functionName = "Test2";
-	tq.Enqueue(msg);
-
-	msg.filePath = "TestFuncsA.dll";
-	msg.functionName = "Test4";
-	tq.Enqueue(msg);
-
-	// While (Hardcoded to be true for now)
-	/*
-	while (true)
+	for (int i = 0; i < THREAD_POOL_MAX; i++)
 	{
-		// Grab the first message off the queue, this is a blocking call
-		Message msgD = tq.Dequeue();
-
-		// Declare string variables for holding DLL and Function Name
-		std::string testDllName = msgD.filePath;
-		std::string testDllFuncName = msgD.functionName;;
-		std::wstring wTestDllName(testDllName.begin(), testDllName.end());
-		LPCWSTR lpcwstrTestDllName = wTestDllName.c_str();
-		LPCSTR lpcstrTestDllFuncName = testDllFuncName.c_str();
-
-		// Instantiate the the child test object
-		ChildTest childTest(log);
-
-		// Get a handle to the test dll
-		HINSTANCE hTestDll = LoadLibrary(lpcwstrTestDllName);
-		if (hTestDll != NULL)
-		{
-			lpcstrTestDllFuncName = testDllFuncName.c_str();
-			testFunc pTestFunc = (testFunc)GetProcAddress(hTestDll, lpcstrTestDllFuncName);
-			if (NULL != pTestFunc)
-			{
-				// Give the function pointer to the child test object
-				childTest.test(pTestFunc);
-			}
-			else
-			{
-				std::cout << "TestHarness: " << testDllName << " Couldn't find test function." << std::endl;
-			}
-			// Free up the DLL after we're done using it
-			FreeLibrary(hTestDll);
-		}
-		else
-		{
-			std::cout << "TestHarness: Could Not find DLL: " << testDllName << std::endl;
-		}
+		std::thread{ childTester, std::ref(testQueue) }.detach();
 	}
-	*/
-	// Grab the first message off the queue, this is a blocking call
-	Message msgD = tq.Dequeue();
-
-	// Declare string variables for holding DLL and Function Name
-	std::string testDllName = msgD.filePath;
-	std::string testDllFuncName = msgD.functionName;
-	thread t1(Thread_Test_obj(), testDllName, testDllFuncName);
-
-	msgD = tq.Dequeue();
-	testDllName = msgD.filePath;
-	testDllFuncName = msgD.functionName;
-	thread t2(Thread_Test_obj(), testDllName, testDllFuncName);
-
-	msgD = tq.Dequeue();
-	testDllName = msgD.filePath;
-	testDllFuncName = msgD.functionName;
-	thread t3(Thread_Test_obj(), testDllName, testDllFuncName);
-
-	// t1.join();
-	// t2.join();
-	// t3.join();
-
-	t1.detach();
-	t2.detach();
-	t3.detach();
-	cout << "<return" << endl;
+	
+	cout << "return" << endl;
 	cin.get();
 
 }
 
+void childTester(ProcessMessageQueue& testQueue)
+{
+	cout << "(" << GetCurrentThreadId() << ") " "started" << endl;
+
+	while (true)
+	{
+		// Grab the first message off the queue, this is a blocking call
+		//cout << GetCurrentThreadId() << ": waiting for test..." << endl;
+		if (testQueue.isEmpty())
+		{
+			Sleep(1000);
+			continue;
+		}
+
+		Message msg = testQueue.Dequeue();
+
+		cout << "(" << GetCurrentThreadId() << ") " << "running test" << endl;
+
+		// Declare string variables for holding DLL and Function Name
+		std::string testDllName = msg.filePath;
+		std::string testDllFuncName = msg.functionName;;
+
+		cout << "(" << GetCurrentThreadId() << ") " << testDllName << endl;
+		cout << "(" << GetCurrentThreadId() << ") " << testDllFuncName << endl;
+
+		// Instantiate the the child test object
+		//ChildTest childTest(NULL); // childTest(log);
+		
+		// get a handle to the test dll
+		HINSTANCE hTestDll = LoadLibraryA(testDllName.c_str());
+		if (hTestDll != NULL)
+		{
+			testFunc pTestFunc = (testFunc)GetProcAddress(hTestDll, testDllFuncName.c_str());
+			if (NULL != pTestFunc)
+			{
+				try
+				{
+					// call the test function
+					pTestFunc();
+				}
+				catch (...)
+				{
+					//mLogger->log(Logger::ERROR_E, "Exception!");
+					cout << "(" << GetCurrentThreadId() << ") " << "exception!" << endl;
+				}
+			}
+			else
+			{
+				std::cout << "(" << GetCurrentThreadId() << ") " << testDllName << " Couldn't find test function." << std::endl;
+			}
+
+			// free up the DLL after we're done using it
+			cout << "(" << GetCurrentThreadId() << ") " << "freeDLL" << endl;
+			FreeLibrary(hTestDll);
+		}
+		else
+		{
+			std::cout << "(" << GetCurrentThreadId() << ") " << "TestHarness: Could Not find DLL: " << testDllName << std::endl;
+		}
+	}
+}
